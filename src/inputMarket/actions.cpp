fe5e9505b07c0bf7116baf3e086ad3b982cb665b
thibaudcathala <thibaud.cathala@epitech.eu>
2024-05-14 16:25:21 +0200

    add: computation of sell or buy amout with weidest value

--- src/inputMarket/actions.cpp
+++ src/inputMarket/actions.cpp
@@ -37,9 +37,17 @@ void Tr::Trade::checkIntersectionMovingAverage()
 void Tr::Trade::analyseOfTheMarket()
 {
     checkIntersectionMovingAverage();
-    std::cerr << "To Buy = " <<
+    this->_signal.percentage =
         (_listCandles.back().stats.bollinger_bands.weirdness * 100.0)
-            * (this->_max_order_percentage / 100.0) << '\n';
+            * (this->_max_order_percentage / 100.0);
+    if (this->_signal.percentage > 0) {
+        this->_signal.percentage = std::abs(this->_signal.percentage);
+        this->_signal.action = Tr::Trade::SELL;
+    } else if (this->_signal.percentage < 0) {
+        this->_signal.action = Tr::Trade::BUY;
+    } else {
+        this->_signal.action = Tr::Trade::PASS;
+    }
 }
 
 void Tr::Trade::manageAnAction()
